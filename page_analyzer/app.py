be39f5a4b5f96f97aea2d6e28971afaafe860f23
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-04-07 11:40:22 +0300

    add index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello, it is page analyzer'
+    return render_template('index.html')
