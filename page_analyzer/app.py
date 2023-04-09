b94a0a5f60aa090a9aea9b0f0a8b588158d7c815
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-04-09 19:02:29 +0300

    connet to database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,6 +4,7 @@ from flask import (
     request
 )
 import os
+import psycopg2
 from dotenv import load_dotenv
 
 
@@ -18,6 +19,9 @@ load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
+# Connect to database
+conn = psycopg2.connect(DATABASE_URL)
+
 
 @app.route('/')
 def index():
@@ -29,3 +33,10 @@ def post_urls():
     url = request.form['url']
     return f'{url}'
 
+
+@app.get('/urls')
+def get_urls():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM urls")
+    records = cursor.fetchall()
+    return f'{records}'
