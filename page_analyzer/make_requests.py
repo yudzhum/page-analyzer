ee6fa223856fc2a337c03023ba8f3f9d07dd5909
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-05-02 23:06:54 +0300

    add check for status

--- page_analyzer/make_requests.py
+++ page_analyzer/make_requests.py
@@ -5,9 +5,11 @@ def safe_request(url):
     result = None
     try:
         req = requests.get(url)
+        if req.status_code == 200:
+            return req
 
     except requests.exceptions.RequestException:
         return result
 
     else:
-        return req
+        return result
